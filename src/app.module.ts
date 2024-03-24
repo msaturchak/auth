aa9f447503b22f1925e4ba3448a03dec9ec807ea
msaturchak <maksym.saturchak03@gmail.com>
2024-03-24 23:50:12 +0200

    feat: remove typeorm configuration for testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from './config/config.service';
-import { AuthModule } from './modules/auth/auth.module';
 import { LocalModule } from './core/local/local.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -15,14 +15,8 @@ import { LocalModule } from './core/local/local.module';
     }),
     ConfigModule,
     TypeOrmModule.forRootAsync({
-      useFactory:
-        process.env.NODE_ENV !== 'test'
-          ? (cfg: ConfigService): TypeOrmModule => ({
+      useFactory:(cfg: ConfigService): TypeOrmModule => ({
               ...cfg.getOrmConfig,
-              autoLoadEntities: true,
-            })
-          : (cfg: ConfigService): TypeOrmModule => ({
-              ...cfg.getOrmConfigTesting,
               autoLoadEntities: true,
             }),
       inject: [ConfigService],
